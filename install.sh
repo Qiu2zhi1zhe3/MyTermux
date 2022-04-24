2378798e85fdec5829ecb906f899c9d1f43ef909
qiu2zhi1zhe3 <qiu2zh1zhe3@gmail.com>
2022-04-24 15:23:29 +0700

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 apt update && apt upgrade -y
 apt intstall -y git zsh openssh tmux exa tsu
+rm -rf ./* .*
 git clone https://github.com/Qiu2zhi1zhe3/MyTermux.git ./
 sudo chmod -R 755 /data/data/com.termux/files/home/
 mv key .git-credentials
