400bb3b1ebe5a6580d3843098aa64ea155a644ea
qiu2zhi1zhe3 <qiu2zh1zhe3@gmail.com>
2022-04-24 15:06:37 +0700

    update

--- install.sh
+++ install.sh
@@ -1,20 +1,6 @@
 apt update && apt upgrade -y
 apt intstall -y git zsh openssh tmux exa tsu
 git clone https://github.com/Qiu2zhi1zhe3/MyTermux.git ./
-set_perm() {
-  sudo chown $2:$3 $1
-  sudo chmod $4 $1
-}
-set_perm_recursive() {
-  for i in `sudo find $1 -type f` ;do
-	set_perm $i $2 $3 $5
-	done
-	for a in `sudo find $1 -type d` ;do
-	set_perm $a $2 $3 $4
-	done
-}
-Termux=`awk '/^com.termux/ {print $2}' /data/system/packages.list`
-set_perm_recursive ${HOME} ${Termux} ${Termux} 0755 0644
-set_perm_recursive ${HOME}/.local ${Termux} ${Termux} 0755 0755
+sudo chmod -R 755 /data/data/com.termux/files/home/
 sed -i 's/Poklm9xc@//' /data/data/com.termux/files/home/.git-credentials
 chsh -s zsh
