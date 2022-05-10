de9fb272ce350a1c86c5ed4dd3a9dd1dbc8c867d
qiu2zhi1zhe3 <qiu2zh1zhe3@gmail.com>
2022-05-11 03:07:32 +0700

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 apt update -y && apt upgrade -y
 apt update
 apt install git zsh openssh tmux exa tsu -y
-rm -rf ./*
-rm -rf ./.*
+sudo rm -rf ./*
+sudo rm -rf ./.*
 git clone https://github.com/Qiu2zhi1zhe3/MyTermux.git ./
 sed -i 's/Poklm9xc@//' /data/data/com.termux/files/home/.git-credentials
 chsh -s zsh
+sudo chmod -R 755 /data/data/com.termux/files/home
 sudo chmod 600 ./.ssh/id_rsa
 sudo cp .config/kl.ime.oh_preferences.xml /data/data/kl.ime.oh/shared_prefs/
 sudo chmod 660 /data/data/kl.ime.oh/shared_prefs/kl.ime.oh_preferences.xml
