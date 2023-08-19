148f269ea8f9f9c6bfd6e8383d8e76c985eba278
qiu2zhi1zhe3 <qiu2zh1zhe3@gmail.com>
2023-08-19 17:41:46 +0700

    update

--- install.sh
+++ install.sh
@@ -10,12 +10,6 @@ sed -i "s/$a//" /data/data/com.termux/files/home/.git-credentials
 chsh -s zsh
 sudo chmod -R 755 /data/data/com.termux/files/home
 sudo chmod 600 ./.ssh/id_rsa
-su -c 'mount -o remount,rw /'
-su -c 'cat << EOF > /.zshrc
-HOME="/data/data/com.termux/files/home"
-source \$HOME/.zshrc
-c
-EOF'
 sudo cp .config/kl.ime.oh_preferences.xml /data/data/kl.ime.oh/shared_prefs/
 sudo chmod 660 /data/data/kl.ime.oh/shared_prefs/kl.ime.oh_preferences.xml
 a=`sudo awk '/^kl.ime.oh/ {print $2}' /data/system/packages.list`
